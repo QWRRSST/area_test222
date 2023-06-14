c5b373ba1e3caf85878f098d9fbade31a0412dc1
QWRRSST <132058570+QWRRSST@users.noreply.github.com>
2023-06-14 10:04:33 +0300

    Изменил код

--- lr3/Program.cs
+++ lr3/Program.cs
@@ -10,9 +10,10 @@ namespace lr3
 
             Console.WriteLine("1. Прямоугольник");
             Console.WriteLine("2. Круг");
-            //Console.WriteLine("3. Треугольник");
-            //Console.WriteLine("4. Трапеция");
-            //Console.WriteLine("5. Сектор");
+            Console.WriteLine("3. Треугольник");
+            Console.WriteLine("4. Трапеция");
+            Console.WriteLine("5. Сектор");
+            Console.WriteLine("6. Прямоугольный треугольник");
 
             string temp = Console.ReadLine();
             int variant = Convert.ToInt32(temp);    // преобразование в число
@@ -33,10 +34,53 @@ namespace lr3
                     Console.Write("Радиус R = ");
                     temp = Console.ReadLine();
                     Rad = Convert.ToDouble(temp);
-                    Console.WriteLine("Площадь круга = " + 4 * Atan(1.0) * Pow(Rad, 2.0));
+                    Console.WriteLine("Площадь круга = " + 4 * Math.Atan(1.0) * Math.Pow(Rad, 2.0));
                     break;
 
-                default: Console.WriteLine("Выбор неверен "); break;
+                case 3:
+                    Console.Write("Высота А = ");
+                    temp = Console.ReadLine();
+                    a = Convert.ToDouble(temp);
+                    Console.Write("Основание B = ");
+                    temp = Console.ReadLine();
+                    b = Convert.ToDouble(temp);
+                    Console.WriteLine("Площадь треугольника = " + (a * b) / 2);
+                    break;
+                case 4:
+                    Console.Write("Основание A = ");
+                    double topBase = double.Parse(Console.ReadLine());
+
+                    Console.Write("Основание B = ");
+                    double bottomBase = double.Parse(Console.ReadLine());
+
+                    Console.Write("Высота h = ");
+                    double height = double.Parse(Console.ReadLine());
+
+                    double area = (topBase + bottomBase) * height / 2;
+
+                    Console.WriteLine("Площадь трапеции = " + area);
+                    break;
+                case 5:
+
+                    Console.Write("Введите угол в градусах: ");
+                    double angleInDegrees = double.Parse(Console.ReadLine());
+
+
+                    Console.Write("Введите радиус: ");
+                    double radius = double.Parse(Console.ReadLine());
+
+
+                    double angleInRadians = Math.PI * angleInDegrees / 180;
+
+
+                    double sectorArea = 0.5 * angleInRadians * Math.Pow(radius, 2);
+
+                    Console.WriteLine("Sector Area: " + sectorArea);
+
+
+
+                    break;
+                
             }
         }
     }
